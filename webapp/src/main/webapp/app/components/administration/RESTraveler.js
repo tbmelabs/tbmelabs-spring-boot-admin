7710e1be07449f619bae253ca7a8fe1ff55cd85b
Timon Borter <timon.borter@iet-gibb.ch>
2017-07-12 11:16:15 +0200

    made the dive-function work. no data displayed at the moment

--- webapp/src/main/webapp/app/components/administration/RESTraveler.js
+++ webapp/src/main/webapp/app/components/administration/RESTraveler.js
@@ -9,7 +9,6 @@ import Grid from 'react-bootstrap/lib/Grid';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
 import Button from 'react-bootstrap/lib/Button';
-import FormGroup from 'react-bootstrap/lib/FormGroup';
 import ControlLabel from 'react-bootstrap/lib/ControlLabel';
 import FormControl from 'react-bootstrap/lib/FormControl';
 
@@ -29,17 +28,14 @@ class RESTraveler extends React.Component {
       errors: {}
     }
 
-    this.onChange = this.onChange.bind(this);
     this.onBack = this.onBack.bind(this);
+    this.onSelect = this.onSelect.bind(this);
+    this.onChange = this.onChange.bind(this);
     this.travel = this.travel.bind(this);
   }
 
   componentWillMount() {
     this.travel(this.state.currentUrl, false);
-  }
-
-  onChange(event) {
-    this.setState({[event.target.name]: event.target.value});
   }
 
   onBack(event) {
@@ -50,12 +46,23 @@ class RESTraveler extends React.Component {
     this.travel(lastLocation, true);
   }
 
+  onSelect(event) {
+    this.travel(event.target.value, false);
+  }
+
+  onChange(event) {
+    this.setState({[event.target.name]: event.target.value});
+  }
+
   travel(location, isStepBack) {
     const history = this.state.history;
 
     this.props.travelTo(location).then(
       response => {
-        const links = response.data._links;
+        const rawLinks = response.data._links;
+        const links = Object.keys(rawLinks).map((key) => {
+          return {[key]: rawLinks[key].href};
+        });
 
         if (!isStepBack) {
           history.push(location);
@@ -72,24 +79,38 @@ class RESTraveler extends React.Component {
     return (
       <Grid>
         <Row>
-          <Col sm={6}>
-            <Button>Back</Button>
+          <CollapsableAlert collapse={!!this.state.errors.form} style='danger' title='An error occured: '
+                            message={this.state.errors.form}/>
+        </Row>
+        <Row>
+          <Col sm={5}>
+            <Button disabled={this.state.history.length === 1} onClick={this.onBack}>Back</Button>
           </Col>
-          <FormGroup>
-            <Col sm={2}>
-              <FormControl componentClass='select' placeholder='Travel to..'>
-                
-              </FormControl>
-            </Col>
-          </FormGroup>
-          <FormGroup>
-            <Col sm={1}>
-              <ControlLabel>Limit:</ControlLabel>
-            </Col>
-            <Col sm={1}>
-              <FormControl name='limit' type='text' value={this.state.limit} onChange={this.onChange}/>
-            </Col>
-          </FormGroup>
+          <Col sm={2}>
+            <FormControl componentClass='select' placeholder='default' onChange={this.onSelect}>
+              <option key='default' value='default'>Travel to..</option>
+              {
+                this.state.links.map((link) => {
+                  return Object.keys(link).map((key) => {
+                    const name = key.charAt(0).toUpperCase() + key.slice(1);
+                    const value = link[key];
+
+                    return (
+                      <option key={key} value={value}>{name}</option>
+                    );
+                  });
+                })
+              }
+            </FormControl>
+          </Col>
+          <Col sm={1}/>
+          <Col sm={1}>
+            <ControlLabel style={{marginTop: '10%'}}>Limit:</ControlLabel>
+          </Col>
+          <Col sm={1}>
+            <FormControl componentClass='input' name='limit' type='text' value={this.state.limit}
+                         onChange={this.onChange}/>
+          </Col>
         </Row>
       </Grid>
     );
