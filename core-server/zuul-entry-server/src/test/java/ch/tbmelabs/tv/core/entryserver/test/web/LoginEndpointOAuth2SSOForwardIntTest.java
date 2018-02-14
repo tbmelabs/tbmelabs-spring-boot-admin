8bbd10a8fed5261489d835aab873d8d22db2afb6
bbortt <timon.borter@gmx.ch>
2018-02-14 16:13:27 +0100

    eureka server is ready for productive build.

--- core-server/zuul-entry-server/src/test/java/ch/tbmelabs/tv/core/entryserver/test/web/LoginEndpointOAuth2SSOForwardIntTest.java
+++ core-server/zuul-entry-server/src/test/java/ch/tbmelabs/tv/core/entryserver/test/web/LoginEndpointOAuth2SSOForwardIntTest.java
@@ -1,4 +1,4 @@
-package ch.tbmelabs.core.servicediscovery.test.web;
+package ch.tbmelabs.tv.core.entryserver.test.web;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -11,11 +11,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MockMvc;
 
-import ch.tbmelabs.core.servicediscovery.test.AbstractEurekaApplicationContextAware;
+import ch.tbmelabs.tv.core.entryserver.test.AbstractZuulApplicationContextAware;
 
-public class WebSecurityOAuth2SSOForwardIntTest extends AbstractEurekaApplicationContextAware {
+public class LoginEndpointOAuth2SSOForwardIntTest extends AbstractZuulApplicationContextAware {
   private static final String FORWARD_HEADER_NAME = "location";
-  private static final String EUREKA_AUTHENTICATOIN_ENTRY_POINT_URI = "http://localhost/login";
+  private static final String ZUUL_AUTHENTICATION_ENTRY_POINT_URI = "http://localhost/login";
   private static final String OAUTH2_AUTHENTICATION_ENTRY_POINT_URI = "http://localhost/oauth/authorize";
 
   @Autowired
@@ -26,16 +26,11 @@ public class WebSecurityOAuth2SSOForwardIntTest extends AbstractEurekaApplicatio
 
   @Test
   public void requestToLoginEndpointShouldForwardToOAuth2AuthorizationEndpoint() throws Exception {
-    String loginForward = mockMvc.perform(get("/")).andDo(print()).andExpect(status().is(HttpStatus.FOUND.value()))
-        .andReturn().getResponse().getHeader(FORWARD_HEADER_NAME);
-
-    assertThat(loginForward).isEqualTo(EUREKA_AUTHENTICATOIN_ENTRY_POINT_URI);
-
     String ssoForward = mockMvc.perform(get("/login")).andDo(print()).andExpect(status().is(HttpStatus.FOUND.value()))
         .andReturn().getResponse().getHeader(FORWARD_HEADER_NAME);
 
     assertThat(ssoForward).startsWith(OAUTH2_AUTHENTICATION_ENTRY_POINT_URI).contains("client_id=" + clientId)
-        .contains("redirect_uri=" + EUREKA_AUTHENTICATOIN_ENTRY_POINT_URI).contains("response_type=code")
+        .contains("redirect_uri=" + ZUUL_AUTHENTICATION_ENTRY_POINT_URI).contains("response_type=code")
         .contains("state=");
   }
 }
