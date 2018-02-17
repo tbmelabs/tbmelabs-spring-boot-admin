a65a482cdb111de0b475233471e305880664a81d
bbortt <timon.borter@gmx.ch>
2018-02-17 09:37:24 +0100

    fix failing unit test.

--- core-server/authorization-server/src/test/java/ch/tbmelabs/tv/core/authorizationserver/test/security/filter/OAuth2BearerTokenAuthenticationFilterTest.java
+++ core-server/authorization-server/src/test/java/ch/tbmelabs/tv/core/authorizationserver/test/security/filter/OAuth2BearerTokenAuthenticationFilterTest.java
@@ -1,20 +1,15 @@
 package ch.tbmelabs.tv.core.authorizationserver.test.security.filter;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.Mockito.doCallRealMethod;
 import static org.mockito.MockitoAnnotations.initMocks;
 
 import java.io.IOException;
 
-import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.Mockito;
+import org.mockito.Spy;
 import org.springframework.security.oauth2.provider.authentication.BearerTokenExtractor;
 import org.springframework.stereotype.Component;
 import org.springframework.test.util.ReflectionTestUtils;
@@ -23,16 +18,12 @@ import org.springframework.web.filter.GenericFilterBean;
 import ch.tbmelabs.tv.core.authorizationserver.security.filter.OAuth2BearerTokenAuthenticationFilter;
 
 public class OAuth2BearerTokenAuthenticationFilterTest {
-  @Mock
+  @Spy
   private OAuth2BearerTokenAuthenticationFilter fixture;
 
   @Before
   public void beforeTestSetUp() throws IOException, ServletException {
     initMocks(this);
-
-    doCallRealMethod().when(fixture).initBean();
-    doCallRealMethod().when(fixture).doFilter(Mockito.any(ServletRequest.class), Mockito.any(ServletResponse.class),
-        Mockito.any(FilterChain.class));
   }
 
   @Test
