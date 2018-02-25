564631d506b0f9c0c88ebc4cbde68c8c1bea8c2f
bbortt <timon.borter@post.ch>
2018-02-25 12:07:53 +0100

    make backend to verify confirmation tokens.

---
+++ core-server/authorization-server/src/main/java/ch/tbmelabs/tv/core/authorizationserver/exception/EmailConfirmationTokenNotFoundException.java
@@ -0,0 +1,11 @@
+package ch.tbmelabs.tv.core.authorizationserver.exception;
+
+import ch.tbmelabs.tv.core.authorizationserver.domain.EmailConfirmationToken;
+
+public class EmailConfirmationTokenNotFoundException extends Throwable {
+  private static final long serialVersionUID = 1L;
+
+  public EmailConfirmationTokenNotFoundException(String token) {
+    super("Unable to find " + EmailConfirmationToken.class + ": " + token);
+  }
+}
--- core-server/authorization-server/src/main/java/ch/tbmelabs/tv/core/authorizationserver/web/signup/SignupConfirmationController.java
+++ core-server/authorization-server/src/main/java/ch/tbmelabs/tv/core/authorizationserver/web/signup/SignupConfirmationController.java
@@ -1,11 +1,39 @@
 package ch.tbmelabs.tv.core.authorizationserver.web.signup;
 
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import ch.tbmelabs.tv.core.authorizationserver.exception.EmailConfirmationTokenNotFoundException;
+import ch.tbmelabs.tv.core.authorizationserver.service.signup.EmailConfirmationTokenService;
 
 @RestController
 @RequestMapping("/signup")
 public class SignupConfirmationController {
-  public static final String CONFIRMATION_ENDPOINT = "/signup/confirm";
-  public static final String TOKEN_ARGUMENT_NAME = "confirmation_token";
+  public static final String CONFIRMATION_ENDPOINT = "/signup/confirm-signup/";
+
+  private static final String CONFIRMATION_ERROR_REDIRECT = "/signup?confirmation_failed";
+  private static final String CONFIRMATION_SUCCESS_REDIRECT = "/signup?confirmation_success";
+
+  @Autowired
+  private EmailConfirmationTokenService emailConfirmationTokenService;
+
+  @GetMapping({ "/confirm-signup/{token}" })
+  public void confirmSignup(HttpServletResponse response, @PathVariable(name = "token", required = true) String token)
+      throws IOException {
+    try {
+      emailConfirmationTokenService.confirmRegistration(token);
+    } catch (EmailConfirmationTokenNotFoundException e) {
+      response.sendRedirect(CONFIRMATION_ERROR_REDIRECT);
+      return;
+    }
+
+    response.sendRedirect(CONFIRMATION_SUCCESS_REDIRECT);
+  }
 }
