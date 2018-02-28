65ee8efc4f356ea5d97a8fec94dd12f4fb6c4174
bbortt <timon.borter@gmx.ch>
2018-02-28 21:00:42 +0100

    refactor karma tests and webpack configuration (v2.0). first try to add frontend coverage in auth server.

--- core-server/authorization-server/src/main/webapp/karma.conf.js
+++ core-server/authorization-server/src/main/webapp/karma.conf.js
@@ -1,53 +1,103 @@
 var webpack = require('karma-webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'public');
+var NODE_DIR = path.resolve(__dirname, 'node_modules');
+var TEST_DIR = path.resolve(__dirname, '__tests__');
+
+process.env.CHROMIUM_BIN = require('puppeteer').executablePath()
+
+const WATCH = process.argv.indexOf('--watch') > -1;
 
 module.exports = function (config) {
   config.set({
-    browsers: ['PhantomJS2'],
+    autoWatch: WATCH,
+    browsers: [
+      'ChromiumHeadlessNoSandbox'
+    ],
+    coverageIstanbulReporter: {
+      fixWebpackSourcePaths: true,
+      reports: [
+        'lcovonly',
+        'html'
+      ]
+    },
+    customLaunchers: {
+      ChromiumHeadlessNoSandbox: {
+        base: 'ChromiumHeadless',
+        flags: [
+          '--no-sandbox'
+        ]
+      }
+    },
     files: [
       'node_modules/babel-polyfill/browser.js',
       'tests.webpack.js'
     ],
-    frameworks: ['chai','mocha'],
-    plugins: [
-      'karma-chai',
-      'karma-coverage',
-      'karma-mocha',
-      'karma-mocha-reporter',
-      'karma-phantomjs2-launcher',
-      'karma-sourcemap-loader',
-      'karma-webpack'
+    frameworks: [
+      'chai',
+      'mocha'
     ],
     preprocessors: {
-      'tests.webpack.js': ['webpack', 'sourcemap']
+      'tests.webpack.js': [
+        'webpack',
+        'sourcemap'
+      ]
     },
-    reporters: ['coverage', 'mocha'],
-    singleRun: true,
+    remapIstanbulReporter: {
+      reports: {
+        lcovonly: '../../../target/test-results/coverage/report-lcov/lcov.info',
+        html: '../../../target/test-results/coverage'
+      }
+    },
+    reporters: ['mocha', 'coverage-istanbul', 'karma-remap-istanbul'],
+    singleRun: !WATCH,
     webpack: {
       devtool: 'inline-source-map',
       module: {
-        loaders: [
+        rules: [
           {
-            test: /\.js$/,
             loader: 'babel-loader',
-            query: {
-              plugins: ['transform-object-rest-spread'],
-              presets: ['env', 'react']
+            test: /\.js$/,
+            options: {
+              plugins: [
+                'transform-object-rest-spread'
+              ],
+              presets: [
+                'env',
+                'react'
+              ]
             }
           }, {
-            test: /\.css$/,
-            loader: "style-loader!css-loader"
+            loader: "style-loader!css-loader",
+            test: /\.css$/
           }, {
-            test: /\.(jpe?g|png|svg|ai)$/,
-            loader: "file-loader?publicPath=public/"
+            loader: "file-loader?publicPath=public/",
+            test: /\.(jpe?g|png|svg|ai)$/
           }, {
-            test: /\.(woff|woff2|eot|ttf)$/,
-            loader: 'url-loader?limit=100000'
+            loader: 'url-loader?limit=100000',
+            test: /\.(woff|woff2|eot|ttf)$/
+          }, {
+            enforce: 'post',
+            exclude: [
+              BUILD_DIR,
+              NODE_DIR,
+              TEST_DIR
+            ],
+            loader: 'istanbul-instrumenter-loader',
+            test: /\.js$/,
+            options: {
+              plugins: [
+                'transform-object-rest-spread'
+              ],
+              presets: [
+                'env',
+                'react'
+              ]
+            }
           }
         ]
       }
-    },
-    webpackServer: {
-      noInfo: true
     }
   });
 };
--- core-server/authorization-server/src/main/webapp/webpack.config.js
+++ core-server/authorization-server/src/main/webapp/webpack.config.js
@@ -1,15 +1,16 @@
 var webpack = require('webpack');
 var path = require('path');
 
-var NODE_DIR = path.resolve(__dirname, 'node_modules');
 var BUILD_DIR = path.resolve(__dirname, 'public');
+var NODE_DIR = path.resolve(__dirname, 'node_modules');
+var TEST_DIR = path.resolve(__dirname, '__tests__');
 
 var COMMON_UTILS = path.resolve(__dirname, 'common');
 var AUTHORIZE_APP = path.resolve(__dirname, 'authorize');
 var SIGNIN_APP = path.resolve(__dirname, 'signin');
 var SIGNUP_APP = path.resolve(__dirname, 'signup');
 
-var config = {
+module.exports = {
   entry: {
     authorize: AUTHORIZE_APP,
     signin: SIGNIN_APP,
@@ -20,37 +21,45 @@ var config = {
     filename: '[name].js'
   },
   module: {
-    loaders: [{
-      test: /\.js$/,
-      exclude: [NODE_DIR],
-      include: [
-        COMMON_UTILS, AUTHORIZE_APP,
-        SIGNIN_APP, SIGNUP_APP],
-      loader: 'babel-loader',
-      query: {
-        plugins: [
-          'transform-object-rest-spread'
+    rules: [
+      {
+        exclude: [
+          NODE_DIR,
+          TEST_DIR
         ],
-        presets: [
-          'env',
-          'react'
-        ]
+        include: [
+          COMMON_UTILS,
+          AUTHORIZE_APP,
+          SIGNIN_APP,
+          SIGNUP_APP
+        ],
+        loader: 'babel-loader',
+        test: /\.js$/,
+        options: {
+          plugins: [
+            'transform-object-rest-spread'
+          ],
+          presets: [
+            'env',
+            'react'
+          ]
+        }
+      }, {
+        loader: "style-loader!css-loader",
+        test: /\.css$/
+      }, {
+        loader: "file-loader?publicPath=public/",
+        test: /\.(jpe?g|png|svg|ai)$/
+      }, {
+        loader: 'url-loader?limit=100000',
+        test: /\.(woff|woff2|eot|ttf)$/
       }
-    }, {
-      test: /\.css$/,
-      loader: "style-loader!css-loader"
-    }, {
-      test: /\.(jpe?g|png|svg|ai)$/,
-      loader: "file-loader?publicPath=public/"
-    }, {
-      test: /\.(woff|woff2|eot|ttf)$/,
-      loader: 'url-loader?limit=100000'
-    }]
+    ]
   },
-  plugins: [new webpack.ProvidePlugin({
-    $: "jquery",
-    jQuery: "jquery"
-  })]
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery"
+    })
+  ]
 };
-
-module.exports = config;
