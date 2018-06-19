27f7a372622e096654190d601267cba3c390898b
bbortt <timon.borter@post.ch>
2018-06-19 21:10:27 +0200

    fixed public resources error.

--- core-server/authorization-server/src/main/java/ch/tbmelabs/tv/core/authorizationserver/web/oauth2/AuthorizationEndpointPublicResourcesController.java
+++ core-server/authorization-server/src/main/java/ch/tbmelabs/tv/core/authorizationserver/web/oauth2/AuthorizationEndpointPublicResourcesController.java
@@ -1,25 +1,27 @@
 package ch.tbmelabs.tv.core.authorizationserver.web.oauth2;
 
-import java.io.File;
 import java.io.IOException;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ResourceLoader;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping(path = {"/oauth/public"})
+@Controller
+@RequestMapping({"/oauth/public"})
 public class AuthorizationEndpointPublicResourcesController {
 
   @Autowired
   private ResourceLoader resourceLoader;
 
-  @GetMapping(path = {"/{filename:(?:\\w|\\W)+\\.(?:js)$}"})
-  public File getPublicJavascriptResource(@PathVariable String filename,
+  @GetMapping(path = {"/{filename:.+}"}, produces = {"application/*"})
+  public void getPublicResource(@PathVariable String filename,
       HttpServletResponse httpServletResponse) throws IOException {
-    return resourceLoader.getResource("classpath:static/public/" + filename).getFile();
+    StreamUtils.copy(
+        resourceLoader.getResource("classpath:static/public/" + filename).getInputStream(),
+        httpServletResponse.getOutputStream());
   }
 }
