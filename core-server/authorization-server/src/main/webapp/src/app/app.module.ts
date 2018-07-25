092f777e271450f4b2e5aa162695885c1d1b8ed9
bbortt <timon.borter@post.ch>
2018-07-25 16:26:59 +0200

    prepare app for server side rendering.

--- core-server/authorization-server/src/main/webapp/src/app/app.module.ts
+++ core-server/authorization-server/src/main/webapp/src/app/app.module.ts
@@ -2,10 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { SigninComponent } from './signin/signin.component';
-import { SignupComponent } from './signup/signup.component';
-import { ConfirmAccessComponent } from './oauth/confirm-access/confirm-access.component';
-import { AppRoutingModule } from './/app-routing.module';
+import { SigninComponent } from './signin';
+import { SignupComponent } from './signup';
+import { ConfirmAccessComponent } from './oauth/confirm-access';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
