23afeef97332edf936f2352156f9c7e82fa7c05a
bbortt <timon.borter@post.ch>
2018-08-03 11:53:24 +0200

    fix frontend tests.

--- core-server/authorization-server/src/main/webapp/src/app/app.component.spec.ts
+++ core-server/authorization-server/src/main/webapp/src/app/app.component.spec.ts
@@ -1,5 +1,5 @@
-import {} from 'jasmine';
 import {async, TestBed} from '@angular/core/testing';
+import {RouterTestingModule} from '@angular/router/testing';
 
 import {AppComponent} from './app.component';
 
@@ -9,6 +9,9 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent
       ],
+      imports: [
+        RouterTestingModule
+      ]
     }).compileComponents();
   }));
   it('should create the app', async(() => {
@@ -25,6 +28,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to tbme-labs-authorization-server!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to TBME Labs');
   }));
 });
