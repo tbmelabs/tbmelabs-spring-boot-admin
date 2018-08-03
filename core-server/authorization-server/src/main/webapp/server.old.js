86a5ff1a846e4664967e8b31fa22e4e2a22bf973
bbortt <timon.borter@post.ch>
2018-08-03 16:18:02 +0200

    fix webpack build.

--- core-server/authorization-server/src/main/webapp/server.old.js
+++ core-server/authorization-server/src/main/webapp/server.old.js
@@ -1,17 +1,18 @@
-import 'zone.js/dist/zone-node';
-import 'reflect-metadata';
+require('zone.js/dist/zone-node');
+require('reflect-metadata');
 
-import * as express from 'express';
-import {join} from 'path';
-import {ngExpressEngine} from '@nguniversal/express-engine';
-import {provideModuleMap} from '@nguniversal/module-map-ngfactory-loader';
+const express = require('express');
+const join = require('path').join;
 
 const app = express();
 
 const PORT = process.env.PORT || 4000;
 const DIST_FOLDER = join(process.cwd(), 'dist');
 
-const {AppServerModuleNgFactory, LAZY_MODULE_MAP} = require('./dist/server/main');
+const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./dist/server/main');
+
+const ngExpressEngine = require('@nguniversal/express-engine').ngExpressEngine;
+const provideModuleMap = require('@nguniversal/module-map-ngfactory-loader').provideModuleMap;
 
 app.engine('html', ngExpressEngine({
   bootstrap: AppServerModuleNgFactory,
@@ -23,15 +24,17 @@ app.engine('html', ngExpressEngine({
 app.set('view engine', 'html');
 app.set('views', join(DIST_FOLDER, 'browser'));
 
-// TODO: proxy backend requests
+// TODO: implement data requests securely
 app.get('/api/*', (req, res) => {
   res.status(404).send('data requests are not supported');
 });
 
+// Serve static files from /browser
 app.get('*.*', express.static(join(DIST_FOLDER, 'browser')));
 
+// All regular routes use the Universal engine
 app.get('*', (req, res) => {
-  res.render('index', {req});
+  res.render('index', { req });
 });
 
 app.listen(PORT, () => {
